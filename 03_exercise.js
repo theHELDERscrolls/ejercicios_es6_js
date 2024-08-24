e79bdc5c35f29c2ac3b41982918c3b1c3ee88b4a
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-08-24 17:29:49 +0200

    Exercise 03: Done 1/3

---
+++ 03_exercise.js
@@ -0,0 +1,31 @@
+// 3.1 Dado el siguiente array, devuelve un array con sus nombres
+// utilizando .map().
+const users = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+
+const allNames = users.map((user) => user.name);
+console.log(allNames);
+
+// 3.2 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que
+// empiece por 'A'.
+const usuarios = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
+];
+
+// 3.3 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y añade al valor de .name el string ' (Visitado)'
+// cuando el valor de la propiedad isVisited = true.
+const cities = [
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: false, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
