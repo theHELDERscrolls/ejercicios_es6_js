cd25dd8cf098e5c24e19a481b72b7f47f40cc832
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-08-25 11:21:54 +0200

    Exercise 04: 0/5

---
+++ 04_exercise.js
@@ -0,0 +1,32 @@
+// 4.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+// 4.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+const edades = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+// 4.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+// 4.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
+// usar la funcion .includes() para la comprobación.
+const youtubers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+// 4.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+// .age sea mayor que 35.
